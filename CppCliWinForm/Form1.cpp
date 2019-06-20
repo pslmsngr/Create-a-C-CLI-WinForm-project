e5c9b7cc0715289d17dc47e5aecdc674cc21720c
Richard Green <pslmsngr@gmail.com>
2019-06-19 19:00:47 -0500

    Used Visual Studio 2019 Community Edition to create new project.

--- CppCliWinForm/Form1.cpp
+++ CppCliWinForm/Form1.cpp
@@ -1,33 +1,3 @@
-#include "stdafx.h"
+#include "pch.h"
 #include "Form1.h"
 
-CppCliWinForm::Form1::Form1(void)
-{
-  InitializeComponent();
-  //
-  //TODO: Add the constructor code here
-  //
-}
-
-CppCliWinForm::Form1::~Form1()
-{
-  if (components)
-  {
-    delete components;
-  }
-}
-
-void CppCliWinForm::Form1::InitializeComponent(void)
-{
-  this->SuspendLayout();
-  // 
-  // Form1
-  // 
-  this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
-  this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-  this->ClientSize = System::Drawing::Size(800, 450);
-  this->Name = L"Form1";
-  this->Text = L"Form1";
-  this->ResumeLayout(false);
-
-}
